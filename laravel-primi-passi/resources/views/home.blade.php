ed38cea47b7de2e5305de0d21548304156e788ca
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-01-30 15:22:14 +0100

    created first page with laravel with a link to a second page

---
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel intro</title>
+</head>
+
+<body>
+    <header>
+        <h1>First Laravel Page</h1>
+    </header>
+    <main>
+        <p>Hello LaraWorld!</p>
+        <h1>{{ $title }} {{ $world }}!</h1>
+        <a href="./welcome">Link to the welcome page!</a>
+    </main>
+</body>
+
+</html>
